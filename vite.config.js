297fadec526004aa5b6f648ca7f06aa00775307a
Bhaskar Thakur <bhaskar4455thakur@gmail.com>
2026-03-15 17:41:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    // This switches the minifier to esbuild, which won't crash on that comment
-    cssMinify: 'esbuild' 
-  }
-})
+import { defineConfig } from 'vite' import react from '@vitejs/plugin-react' // https://vite.dev/config/ export default defineConfig({ plugins: [react()], })
