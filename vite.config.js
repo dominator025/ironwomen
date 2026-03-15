4af3d7e38aa263796c481e2ca16c70284cc6beb3
Bhaskar Thakur <bhaskar4455thakur@gmail.com>
2026-03-15 17:21:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This tells Vite to use esbuild instead of lightningcss
+    cssMinify: 'esbuild' 
+  }
 })
